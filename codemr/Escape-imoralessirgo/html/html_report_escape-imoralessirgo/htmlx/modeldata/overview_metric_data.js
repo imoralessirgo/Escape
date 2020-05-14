674edbd81b3da4d3df6f1ae0169431921eebc381
imoralessirgo <imoralessirgo@outlook.com>
2020-05-13 22:09:58 -0400

    codemr

---
+++ codemr/Escape-imoralessirgo/html/html_report_escape-imoralessirgo/htmlx/modeldata/overview_metric_data.js
@@ -0,0 +1,1 @@
+var CodeMR_ProjectMetrics = {"Project Name":"Escape-imoralessirgo working set: {Escape-imoralessirgo}", "Number of Highly Problematic Classes":0, "Number of Entities":36, "Number of Problematic Classes":0, "Class Lines of Code":1518, "Number of External Packages":6, "Number of Packages":9, "Number of External Entities":23}
---
+++ codemr/Escape-imoralessirgo/html/html_report_escape-imoralessirgo/htmlx/modeldata/packagedependency.js
@@ -0,0 +1,29 @@
+var matrix = [[0,0,0,5,0,1,0,4,3],[0,0,0,1,0,0,1,0,0],[3,0,0,3,0,3,0,3,3],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[1,0,0,2,1,0,0,2,0],[4,0,3,5,4,5,0,4,4],[0,0,0,0,0,0,0,0,3],[0,0,0,0,0,0,0,0,0]];
+var packages = [{
+"name": " escape.board", "color": " #3182bd"
+}
+,{
+"name": " escape.hex", "color": " #6baed6"
+}
+,{
+"name": " escape.pathfind", "color": " #9ecae1"
+}
+,{
+"name": " escape.piece", "color": " #c6dbef"
+}
+,{
+"name": " escape.rule", "color": " #e6550d"
+}
+,{
+"name": " escape.util", "color": " #fd8d3c"
+}
+,{
+"name": " escape", "color": " #fdae6b"
+}
+,{
+"name": " escape.board.coordinate", "color": " #fdd0a2"
+}
+,{
+"name": " escape.exception", "color": " #31a354"
+}
+];
